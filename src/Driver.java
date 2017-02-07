2fabfc34e0b25344bdb6bdf490319e28926ffa5c
Andrew Hill <andrew.2.hill@ucdenver.edu>
2017-02-06 21:02:59 -0700

    Added code for Backtracking and Iterative Backtracking. Also added some new test cases.

--- src/Driver.java
+++ src/Driver.java
@@ -8,17 +8,29 @@ public class Driver {
     public static void main(String[] args){
         JSON_Parse parser = new JSON_Parse();
         Game game;
-        State s = new State();
         try {
             game = parser.parseFromFile(args[0]);
             if(game != null && game.validate()){
                 System.out.println("Valid game received.");
             }
+            System.out.println("Beginning Backtracking Depth-First Search");
+            Player p = new Player();
+            //p.printGameState(game.start);
+            p.playGame(game);
+            System.out.println("Solution found in:" + p.previous_moves.size());
+            for(State s : p.previous_moves){
+                p.printGameState(s);
+            }
+            p.playGameIterative(game);
+            System.out.println("Solution found in:" + p.previous_moves.size());
+            for(State s: p.previous_moves){
+                p.printGameState(s);
+            }
         }catch(FileNotFoundException ex){
             System.out.println("File not found. Please try again.");
         }
-        Player p = new Player();
-        ArrayList<int[]> possibleMoves = p.getApplicableMoves(s);
+
+
     }
 
 }
--- src/Player.java
+++ src/Player.java
@@ -1,9 +1,14 @@
-import java.util.ArrayList;
+import java.lang.reflect.Array;
+import java.util.*;
 
 /**
  * Created by Andrew on 2/6/2017.
  */
 public class Player {
+    public LinkedList<State> previous_moves;
+    Player(){
+        previous_moves = new LinkedList<>();
+    }
     public boolean checkRule(iRule r, State s){
         return r.isApplicable(s);
     }
@@ -24,11 +29,64 @@ public class Player {
         return moves;
     }
     public State executeMove(int[] move, State s){
-        int tempVal = s.state[s.zeroRow+move[0]][s.zeroCol+move[1]];
-        s.state[s.zeroRow+move[0]][s.zeroCol+move[1]] = 0;
-        s.state[s.zeroRow][s.zeroCol] = tempVal;
-        s.zeroRow = s.zeroRow+move[0];
-        s.zeroCol = s.zeroCol+move[1];
-        return s;
+        State newState = new State(s);
+        int tempVal = newState.state[newState.zeroRow+move[0]][newState.zeroCol+move[1]];
+        newState.state[newState.zeroRow+move[0]][newState.zeroCol+move[1]] = 0;
+        newState.state[newState.zeroRow][newState.zeroCol] = tempVal;
+        newState.zeroRow = newState.zeroRow+move[0];
+        newState.zeroCol = newState.zeroCol+move[1];
+        return newState;
+    }
+    public boolean playGame(Game g){
+        previous_moves.clear();
+        previous_moves.add(g.start);
+        return playGame(previous_moves,g.goal,30);
+    }
+    public boolean playGameIterative(Game g){
+        previous_moves.clear();
+        previous_moves.add(g.start);
+        int bound = 1;
+        while(bound < 30 && !playGame(previous_moves,g.goal,bound)){
+            System.out.println("Iterative Backtracking at Depth: " + bound);
+            previous_moves.clear();
+            previous_moves.add(g.start);
+            ++bound;
+        }
+        return true;
+    }
+    public boolean playGame(LinkedList<State> prev_moves, State target,int depthBound){
+
+        if(prev_moves.size() > depthBound){
+            return false;
+        }
+        // We have found the correct path. End here.
+        if(prev_moves.peekLast().equals(target)){
+            //printGameState(prev_moves.peekLast());
+            System.out.println("Solution found in: "+ prev_moves.size());
+            return true;
+        }else{
+            // For each candidate move in the move set:
+            State lastMove = prev_moves.getLast();
+            ArrayList<int[]> moves = getApplicableMoves(lastMove);
+            for(int[] candidate : moves){
+                State new_state = executeMove(candidate,lastMove);
+                if(!prev_moves.contains(new_state)) {
+                    prev_moves.add(new_state);
+                    //printGameState(new_state);
+                    if (playGame(prev_moves, target,depthBound)) {
+                        return true;
+                    }else{
+                        prev_moves.removeLast();
+                    }
+                }
+            }
+            return false;
+        }
+    }
+    public void printGameState(State s){
+        for(int[] row: s.state){
+            System.out.println(Arrays.toString(row));
+        }
+        System.out.println();
     }
 }
